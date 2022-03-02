f77cd8d9a3b63f9b8c9b402f512f00daf2b2a1bd
yvahosc <yvahosc@unal.edu.co>
2022-03-02 07:12:41 -0500

    Creación de documentación
    Creación de documentación agregando comentarios en los archivos controladores y vistas de las rutas creadas dentro de la aplicación.

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/**
+ * Creación del controlador de ruta inicial de la aplicación.
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Tutorial de Nodejs' });
 });
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/**
+ * Creación del controlador de la ruta users de la aplicación.
+ */
 router.get('/', function(req, res, next) {
-  res.render('users',{title: 'Users', message: 'Respond with a resource'});
+  res.render('users',{title: 'Users', message: 
+  '¡Hola! ¿Quieres saber algo?'});
 });
 
+/**
+ * Creación del controlador de la ruta users/cool de la aplicación.
+ */
 router.get('/cool', function(req, res, next) {
-  res.render('users',{title: 'Users - Cool', message: '¡Eres genial!'});
+  res.render('usersCool',{title: 'Users - Cool', message: '¡Eres genial!'});
 });
 
 module.exports = router;
